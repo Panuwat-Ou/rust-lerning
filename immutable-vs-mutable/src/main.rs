02ca899ae4dbd6eb1c6d748af720b9b14f465cfc
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-18 21:57:04 +0700

    immutable-mutable

---
+++ immutable-vs-mutable/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut panuwat_energy = 100;
+    println!("Panuwat's energy: {}", panuwat_energy);
+    panuwat_energy = 200;
+    println!("Panuwat's energy: {}", panuwat_energy);
+}
