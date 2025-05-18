01ff512c612aa41f2b0f58cb10d560418cbcc6ad
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-18 22:46:54 +0700

    if else match loop

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,12 +1,31 @@
 fn main() {
-let mut apple = 0;
-loop {
-
-    apple += 1;
-    println!("Apple count: {}", apple);
-    if apple == 5 {
-        break;
+    let weather = "rainy";
+    if weather == "sunny" {
+        println!("It's a sunny day!");
+    } else if weather == "rainy" {
+        println!("It's a rainy day!");
+    } else if weather == "stromy" {
+        println!("Weather is stromy!!");
+    } else {
+        println!("Weather is unknown!");
     }
-   
-}
+
+    let enemy = "dragon";
+    match enemy {
+        "goblin" => println!("A goblin appears!"),
+        "troll" => println!("A troll appears!"),
+        "dragon" => println!("A dragon appears!"),
+        "skeleton" => println!("A skeleton appears!"),
+        _ => println!("An unknown enemy appears!"),
+    }
+    let mut wood = 0;
+    loop {
+        if wood < 10 {
+            println!("Gathering wood...{}", wood);
+            wood += 1;
+        } else {
+            println!("Wood limit reached! You have {} wood.", wood);
+            break;
+        }
+    }
 }
