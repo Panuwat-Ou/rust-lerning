4ba8d1f1544019df1e400afc6ac231f3871e8a97
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-19 21:41:37 +0700

    enum

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -10,8 +10,31 @@ fn main()
     println!("{}'s health: {}", player.name, player.health);
     player.health(250);
     println!("{}'s health: {}", player.name, player.health);
+
+    let current_state = player_state::Collecting(5);
+    match current_state
+    {
+        player_state::Resting => println!("Player is resting"),
+        player_state::Fighting => println!("Player is attacking"),
+        player_state::Collecting(amount) => println!("Player is collecting {} items", amount),
+        player_state::Defending => println!("Player is defending"),
+    }
+
+    let player_state_collecting = example_state::Collecting(5);
+    player_state_collecting.describe();
+    let player_state_fighting = example_state::Fighting;
+    let player_state_defending = example_state::Defending;
+    player_state_fighting.describe();
+    player_state_defending.describe();
 }
 
+enum player_state
+{
+    Resting,
+    Fighting,
+    Collecting(u32),
+    Defending,
+}
 struct warrior
 {
     name: String,
@@ -33,4 +56,40 @@ impl warrior
     {
         self.health = self.health.saturating_sub(damage);
     }
+}
+
+impl player_state
+{
+    fn describe(&self) 
+    {
+        match self
+        {
+            player_state::Resting => println!("Player is resting"),
+            player_state::Fighting => println!("Player is attacking"),
+            player_state::Collecting(amount) => println!("Player is collecting {} items", amount),
+            player_state::Defending => println!("Player is defending"),
+        }
+    }
+    
+}
+
+enum example_state
+{
+    Fighting,
+    Collecting(u32),
+    Defending,
+}
+
+impl example_state
+{
+    fn describe(&self) 
+    {
+        match self
+        {
+            example_state::Fighting => println!("Player is attacking"),
+            example_state::Collecting(amount) => println!("Player is collecting {} items", amount),
+            example_state::Defending => println!("Player is defending"),
+        }
+    }
+    
 }
