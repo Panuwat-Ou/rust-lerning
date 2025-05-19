aa8d5765cb44127873ad95117ba52acddb141755
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-19 21:26:13 +0700

    struct

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,25 +1,36 @@
-// fn main() {
-//     let treasure1 = "Gold coins";
-//     let treasure2= "silver coins";
-//     let Result = longest_treasure(treasure1, treasure2);
-//     println!("the longest treasure is: {}", Result)
-// }
-// fn longest_treasure<'a> (x: &'a str, y: &'a str) -> &'a str{
-//     if x.len() > y.len()
-//     {
-//         x
-//     } else { y }
-// }
-fn longest_map<'a>(map1: &'a str ,map2: &'a str) -> &'a str{
-    if map1.len() > map2.len()
-    {
-        map1
-    } else { map2 }
-}
 fn main()
 {
-    let map1 = "Ancient Map of the sea";
-    let map2 = "Map to hidden gold";
-    let chosen_map = longest_map(map1,map2);
-    println!("Crabby's longest map: {}",chosen_map);
+    let mut player = warrior
+    {
+        name: String::from("Player"),
+        health: 100,
+    };
+
+    player.take_damage(20);
+    println!("{}'s health: {}", player.name, player.health);
+    player.health(250);
+    println!("{}'s health: {}", player.name, player.health);
+}
+
+struct warrior
+{
+    name: String,
+    health: u8,
+}
+
+impl warrior
+{
+    fn health(&mut self, heal: u8)
+    {
+        if self.health.saturating_add(heal) >= 100
+        {
+            self.health = 100;
+            return;
+        }
+    }
+
+    fn take_damage(&mut self, damage: u8)
+    {
+        self.health = self.health.saturating_sub(damage);
+    }
 }
