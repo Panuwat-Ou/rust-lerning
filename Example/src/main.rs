cebf93dca8122dbcc9f59e9a87a5fcccb48f315a
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-19 22:27:53 +0700

    trait & gemeric

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,95 +1,35 @@
+struct Inventory<T> {
+    item: T,
+}
 fn main()
 {
-    let mut player = warrior
-    {
-        name: String::from("Player"),
-        health: 100,
-    };
+    let gold_inventory = Inventory { item: 100 };
+    let sword_inventory = Inventory { item: "Sword" };
 
-    player.take_damage(20);
-    println!("{}'s health: {}", player.name, player.health);
-    player.health(250);
-    println!("{}'s health: {}", player.name, player.health);
-
-    let current_state = player_state::Collecting(5);
-    match current_state
-    {
-        player_state::Resting => println!("Player is resting"),
-        player_state::Fighting => println!("Player is attacking"),
-        player_state::Collecting(amount) => println!("Player is collecting {} items", amount),
-        player_state::Defending => println!("Player is defending"),
-    }
-
-    let player_state_collecting = example_state::Collecting(5);
-    player_state_collecting.describe();
-    let player_state_fighting = example_state::Fighting;
-    let player_state_defending = example_state::Defending;
-    player_state_fighting.describe();
-    player_state_defending.describe();
+    println!("Hold Gold: {}", gold_inventory.item);
+    println!("Hold Sword: {}", sword_inventory.item);
+    sword_inventory.display();
+    gold_inventory.display();
 }
 
-enum player_state
-{
-    Resting,
-    Fighting,
-    Collecting(u32),
-    Defending,
-}
-struct warrior
-{
-    name: String,
-    health: u8,
-}
-
-impl warrior
-{
-    fn health(&mut self, heal: u8)
-    {
-        if self.health.saturating_add(heal) >= 100
-        {
-            self.health = 100;
-            return;
-        }
-    }
-
-    fn take_damage(&mut self, damage: u8)
-    {
-        self.health = self.health.saturating_sub(damage);
-    }
-}
-
-impl player_state
-{
-    fn describe(&self) 
-    {
-        match self
-        {
-            player_state::Resting => println!("Player is resting"),
-            player_state::Fighting => println!("Player is attacking"),
-            player_state::Collecting(amount) => println!("Player is collecting {} items", amount),
-            player_state::Defending => println!("Player is defending"),
-        }
-    }
+trait DisplayItem {
+    fn display(&self);
     
 }
 
-enum example_state
-{
-    Fighting,
-    Collecting(u32),
-    Defending,
+trait DisplayItemV2 {
+    fn displayV2(&self);
+    
 }
 
-impl example_state
-{
-    fn describe(&self) 
-    {
-        match self
-        {
-            example_state::Fighting => println!("Player is attacking"),
-            example_state::Collecting(amount) => println!("Player is collecting {} items", amount),
-            example_state::Defending => println!("Player is defending"),
-        }
+impl <T: std::fmt::Debug> DisplayItem for Inventory<T> {
+    fn display(&self) {
+        println!("Inventory contains: {:?}", self.item);
     }
-    
+}
+impl <T> DisplayItemV2 for Inventory<T> 
+where T: std::fmt::Debug {
+    fn displayV2(&self) {
+        println!("Inventory contains: {:?}", self.item);
+    }
 }
