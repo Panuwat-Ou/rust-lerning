19832a54be4b52823d134bb2534c26d3a5bf2a5f
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-19 21:13:01 +0700

    life time

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,11 +1,25 @@
-fn main() {
-    let mut treasure = String::from("Gold Coin");
-    let friend1 = &treasure;
-    let friend2 = &treasure;
-    println!("Friend 1 see:{}",friend1);
-    println!("Friend 2 see:{}",friend2);
-
-    let trusted_friend = &mut treasure;
-    trusted_friend.push_str(" and Silver coin");
-    println!("Trusted friend update:{}", trusted_friend);
+// fn main() {
+//     let treasure1 = "Gold coins";
+//     let treasure2= "silver coins";
+//     let Result = longest_treasure(treasure1, treasure2);
+//     println!("the longest treasure is: {}", Result)
+// }
+// fn longest_treasure<'a> (x: &'a str, y: &'a str) -> &'a str{
+//     if x.len() > y.len()
+//     {
+//         x
+//     } else { y }
+// }
+fn longest_map<'a>(map1: &'a str ,map2: &'a str) -> &'a str{
+    if map1.len() > map2.len()
+    {
+        map1
+    } else { map2 }
+}
+fn main()
+{
+    let map1 = "Ancient Map of the sea";
+    let map2 = "Map to hidden gold";
+    let chosen_map = longest_map(map1,map2);
+    println!("Crabby's longest map: {}",chosen_map);
 }
