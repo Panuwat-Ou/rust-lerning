adb8bd6f5fc796277541df86fe33273ac1317374
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-19 21:12:41 +0700

    Borrow

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,33 +1,11 @@
-use std::fmt::format;
-
 fn main() {
-    // Function calls
-    greet();
-    greet_user("Alice");
-    let result = sum(5, 10);
-    println!("Sum: {}", result);
-    let result = greet_user_reply("Bob");
-    println!("{}", result);
-    let result = user_task("exercise", 30);
-    println!("{}", result);
-}
+    let mut treasure = String::from("Gold Coin");
+    let friend1 = &treasure;
+    let friend2 = &treasure;
+    println!("Friend 1 see:{}",friend1);
+    println!("Friend 2 see:{}",friend2);
 
-fn greet() {
-    println!("Hello!");
-}
-
-fn greet_user(name: &str) {
-    println!("Hello, {}!", name)
-}
-
-fn sum(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-fn greet_user_reply(name: &str) -> String {
-    format!("Hello, {}!", name)
-}
-
-fn user_task(task: &str,time :i32) -> String {
-    format!("doing {} for {} minutes",task,time)
+    let trusted_friend = &mut treasure;
+    trusted_friend.push_str(" and Silver coin");
+    println!("Trusted friend update:{}", trusted_friend);
 }
