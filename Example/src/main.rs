a4438b04ca2f6842df8ac260697b567ac047004e
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-21 00:02:26 +0700

    String and &str

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,35 +1,9 @@
-struct Inventory<T> {
-    item: T,
-}
 fn main()
 {
-    let gold_inventory = Inventory { item: 100 };
-    let sword_inventory = Inventory { item: "Sword" };
+  let map = String::from("Old map");
 
-    println!("Hold Gold: {}", gold_inventory.item);
-    println!("Hold Sword: {}", sword_inventory.item);
-    sword_inventory.display();
-    gold_inventory.display();
-}
-
-trait DisplayItem {
-    fn display(&self);
-    
-}
-
-trait DisplayItemV2 {
-    fn displayV2(&self);
-    
-}
-
-impl <T: std::fmt::Debug> DisplayItem for Inventory<T> {
-    fn display(&self) {
-        println!("Inventory contains: {:?}", self.item);
-    }
-}
-impl <T> DisplayItemV2 for Inventory<T> 
-where T: std::fmt::Debug {
-    fn displayV2(&self) {
-        println!("Inventory contains: {:?}", self.item);
-    }
+  let borrow_map = &map;
+  let mut converted_map = borrow_map.to_string();
+  converted_map.push_str(" to new map");
+  println!("Converted map: {}", converted_map);
 }
