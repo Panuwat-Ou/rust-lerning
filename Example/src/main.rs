ebf792bda9e0097e450f2927ff33c8d2dc602dca
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-23 23:59:22 +0700

    Vector

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,18 +1,56 @@
-fn main()
-{
-  let treasures = ["Gold", "Silver", "Bronze", "Diamond","Emerald","Ruby"];
-  let mut energy = 5;
+// fn main()
+// {
+//   let mut treasure: Vec<String>  = Vec::new();
+//   treasure.push(String::from("gold"));
+//   treasure.push(String::from("silver"));
 
-  for treasure in treasures.iter(){
-    if energy <= 0 { 
-      println!("Out of energy");
-      break;
-    } else if treasure == &"Ruby" {
-      println!("Found a Ruby! {}", treasure);
-      break;
-    } else {
-      energy -= 1;
-      println!("Found a treasure! {}, energy : {}", treasure, energy);
-    }
+//   treasure.pop();
+//   treasure.remove(0);
+  
+//   let mut treasur2 = vec!["gold", "silver","gemstones"];
+
+//   treasur2.len(); // 3 ปัจจุบันมี 3 อัน
+//   treasur2.capacity(); // 3 จุได้เท่าไหร่
+//   treasur2.pop();
+//   treasur2.len(); // 2 ปัจจุบันมี 2 อัน
+//   treasur2.capacity();// 3 จุได้เท่าไหร่
+
+// }
+
+// fn main()
+// {  
+//   //let mut item = vec!["gold", "silver","gemstones","emeralds"];
+//   let mut item = vec!["gold"];
+//   println!("item capacity is: {}", item.capacity());
+//    item.push("diamond");
+//   println!("item capacity is: {}", item.capacity());
+//    item.push("platinum");
+//   println!("item capacity is: {}", item.capacity());
+//    item.push("gold2");
+
+//   println!("item capacity is: {}", item.capacity());
+//    item.push("silver2");
+//   println!("item capacity is: {}", item.capacity());
+//    item.push("gemstones2");
+//     println!("item capacity is: {}", item.capacity());
+//    item.pop();
+
+//   println!("The treasure is: {:?}", item);
+//   println!("item length is: {}", item.len());
+//   println!("item capacity is: {}", item.capacity());
+// }
+fn main()
+{  
+  //let mut item = vec!["gold", "silver","gemstones","emeralds"];
+  let mut item :Vec<String> = Vec::new();
+  println!("item capacity start is: {}", item.capacity());
+  let mut count = 0;
+  while count < 20 {
+    println!("push item : {}", format!("Gold{}", count+1));
+    item.push(format!("Gold{}", count+1));
+    println!("item length is: {}", item.len());
+    println!("item capacity is: {}", item.capacity());
+    println!("================================================");
+    count += 1;
   }
 }
