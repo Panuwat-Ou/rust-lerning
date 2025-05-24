0d775ecba35cd80fcc57854f16175493fa6a5602
Panuwat-ou <83071175+Panuwat-Ou@users.noreply.github.com>
2025-05-24 23:57:04 +0700

    Iterators & Closures

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,56 +1,7 @@
-// fn main()
-// {
-//   let mut treasure: Vec<String>  = Vec::new();
-//   treasure.push(String::from("gold"));
-//   treasure.push(String::from("silver"));
-
-//   treasure.pop();
-//   treasure.remove(0);
-  
-//   let mut treasur2 = vec!["gold", "silver","gemstones"];
-
-//   treasur2.len(); // 3 ปัจจุบันมี 3 อัน
-//   treasur2.capacity(); // 3 จุได้เท่าไหร่
-//   treasur2.pop();
-//   treasur2.len(); // 2 ปัจจุบันมี 2 อัน
-//   treasur2.capacity();// 3 จุได้เท่าไหร่
-
-// }
-
-// fn main()
-// {  
-//   //let mut item = vec!["gold", "silver","gemstones","emeralds"];
-//   let mut item = vec!["gold"];
-//   println!("item capacity is: {}", item.capacity());
-//    item.push("diamond");
-//   println!("item capacity is: {}", item.capacity());
-//    item.push("platinum");
-//   println!("item capacity is: {}", item.capacity());
-//    item.push("gold2");
-
-//   println!("item capacity is: {}", item.capacity());
-//    item.push("silver2");
-//   println!("item capacity is: {}", item.capacity());
-//    item.push("gemstones2");
-//     println!("item capacity is: {}", item.capacity());
-//    item.pop();
-
-//   println!("The treasure is: {:?}", item);
-//   println!("item length is: {}", item.len());
-//   println!("item capacity is: {}", item.capacity());
-// }
 fn main()
 {  
-  //let mut item = vec!["gold", "silver","gemstones","emeralds"];
-  let mut item :Vec<String> = Vec::new();
-  println!("item capacity start is: {}", item.capacity());
-  let mut count = 0;
-  while count < 20 {
-    println!("push item : {}", format!("Gold{}", count+1));
-    item.push(format!("Gold{}", count+1));
-    println!("item length is: {}", item.len());
-    println!("item capacity is: {}", item.capacity());
-    println!("================================================");
-    count += 1;
-  }
+  let  treasures = vec![100, 200, 300, 400, 500];
+  let double : Vec<i32>= treasures.iter().map(|x| x * 2 ).collect();
+  println!("Treasures: {:?}", treasures);
+  println!("Doubled Treasures: {:?}", double);
 }
